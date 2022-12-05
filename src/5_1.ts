c7fcd40d967912b2d855599a1ab03f945420487a
Koban Davis <kobandavis@gmail.com>
2022-12-05 09:01:19 +0000

    solve day 5

---
+++ src/5_1.ts
@@ -0,0 +1,46 @@
+import { useFile } from "./help";
+
+type Move = [number, number, number]
+
+const [diagram, moveset] = useFile('5.txt').split('\n\n')
+
+const parseDiagram = (diagram: string) => {
+    const lines = diagram.split('\n')
+    lines.splice(lines.length - 1, 1)
+    const offset = 4
+    const len = (lines[0].length + 1) / offset
+    const crates = new Array(len).fill(0).map(() => [])
+
+    lines.reverse().forEach(line => {
+        for (let i = 0; i < len; i++) {
+            const value = line.charAt(i * offset + 1) // + 1 for [ bracket
+            if (value !== ' ') {
+                crates[i].push(value)
+            }
+        }
+    })
+
+    return crates
+}
+
+const parseMoveset = (moveset: string) => {
+    const stringMoves = moveset.split('\n')
+    return stringMoves.map(move => {
+        const match = [...move.matchAll(/move (\d+) from (\d+) to (\d+)/g)][0]
+        return match.slice(1, 4).map(Number) as Move
+    })
+}
+
+const followMove = ([count, fromCrate, toCrate]: Move) => {
+    for (let i = 0; i < count; i++) {
+        crates[toCrate - 1].push(crates[fromCrate - 1].pop())
+    }
+}
+
+const crates = parseDiagram(diagram)
+const moves = parseMoveset(moveset)
+
+moves.forEach(followMove)
+
+const message = crates.reduce((message, crate) => message + crate.pop(), '')
+console.log(message)
