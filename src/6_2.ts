33f5495651709bb0fce2c5b76fb8422a7c7d47fd
Koban Davis <koban.davis@mondago.com>
2022-12-06 08:23:56 +0000

    life is good when part 2 is a magic number change

---
+++ src/6_2.ts
@@ -0,0 +1,12 @@
+import { useFile } from './help'
+
+const input = useFile('6.txt')
+const packetLength = 14
+
+for (let i = packetLength; i < input.length; i++) {
+	const unique = new Set(input.slice(i - packetLength, i))
+	if (unique.size === packetLength) {
+		console.log(i)
+		break
+	}
+}
